666fb0e625819d169ad3f9cf5aa97db8d2ada86d
AgusM01 <merino.agustin.01@gmail.com>
2025-03-31 22:24:07 -0300

    Comienzo con el main.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,44 @@
 module Main (main) where
 
-import Lib
+import            System.Console.GetOpt
+import            Parser                ( parseComm )
+import            Lib
+import qualified  System.Environment    as Env 
+import qualified  Eval                  as E
+
+-------------------------------------------------------
+
+data Options = Options
+    { optAST  :: Bool,
+      optEval :: PlotList,
+      optHelp :: Bool
+    } deriving Show
+
+defaultOptions :: Options 
+defaultOptions = Options { optAST = False, optEvaL = [], optHelp = False }
+
+options :: [OptDescr (Options -> Options)]
+options =
+    [ Option    ['a']
+                ["AST"]
+                (NoArg (\opts -> opts { optAST = True }))
+                "Mostrar el AST del programa de entrada."
+    , Option    ['e']
+                ["evaluator"]
+                (NoArg (\opts -> opts { optEval = True }))
+                "Evaluar el programa de entrada."
+    , Option    ['h']
+                ["help"]
+                (NoArg (\opts -> opts { optHelp = True }))
+                "Imprimir guia de uso."
+  ]
+
+
+ 
+        
+
+
+
 
 main :: IO ()
 main = ...
