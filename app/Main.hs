63237981ce13d084ae68484270152edf8b169516
AgusM01 <merino.agustin.01@gmail.com>
2025-04-03 23:07:17 -0300

    Continuo Main, hacer runOpts

--- app/Main.hs
+++ app/Main.hs
@@ -8,15 +8,21 @@ import qualified  Eval                  as E
 
 -------------------------------------------------------
 
-data Options = Options
+data Options = Options -- Define las opciones utilizando un registro.
     { optAST  :: Bool,
-      optEval :: PlotList,
+      optEval :: Bool,
       optHelp :: Bool
     } deriving Show
 
 defaultOptions :: Options 
-defaultOptions = Options { optAST = False, optEvaL = [], optHelp = False }
+defaultOptions = Options { optAST = False, optEvaL = False, optHelp = False }
 
+-- Cada función sirve para actualizar el campo:
+-- (\opts -> opts { optAST = True }) (Options False [] False) 
+-- (Options False [] False) { optAST = True }
+--  En Haskell, la notación de actualización de registros { campo = nuevoValor } crea una nueva estructura con el campo modificado.
+-- Esto significa que el nuevo valor es:
+-- Options { optAST = True, optEval = [], optHelp = False }
 options :: [OptDescr (Options -> Options)]
 options =
     [ Option    ['a']
@@ -34,11 +40,28 @@ options =
   ]
 
 
- 
-        
+-- getOpt :: ArgOrder a -> [OptDescr a] -> [String] -> ([a], [String], [String])
+-- [a] es una lista de funciones que transforman la configuración del programa.
+-- (cuando a es de tipo (Options -> Options)
+-- [String]: Argumentos que no fueron reconocidos como opciones.
+-- [String]: Lista de errores si hubo problemas con la sintaxis de las opciones.
+-- 
+-- Permute permite que las opciones aparezcan en cualquier orden.
+-- flip intercambia los argumentos de una funcion binaria: flip f x y = f y x 
+-- foldl (flip id) defaultOptions o = (flip id) Options { optAST = False, optEvaL = False, optHelp = False } (\opts -> opts { optAST = True })
+-- foldl (flip id) defaultOptions o = id (\opts -> opts { optAST = True }) (Options { optAST = False, optEvaL = False, optHelp = False })
+-- y así le va cambiando las opciones.
+-- Finalmente encapsula todo en IO.
+finalOptions :: [String] -> IO (Options, [String])
+finalOptions argv = case getOpt Permute options argv of
+  (o, n, []  ) -> return (foldl (flip id) defaultOptions o, n)
+  (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "Uso:"
 
-
-
-
+-- Computation getArgs returns a list of the program's command line arguments (not including the program name).
 main :: IO ()
-main = ...
+main = do 
+    s : opts <- Env.getArgs 
+    (opts', _) <- finalOptions opts
+    runOptions s opts'
+
